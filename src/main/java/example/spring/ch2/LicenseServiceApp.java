64bfe08699e9a24085df05b92a53fec2d9972d48
w22116972 <w22116972@gmail.com>
2020-03-13 20:31:05 +0800

    spring example ch2

---
+++ src/main/java/example/spring/ch2/LicenseServiceApp.java
@@ -0,0 +1,11 @@
+package example.spring.ch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LicenseServiceApp.class, args);
+    }
+}
